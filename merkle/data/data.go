6bbc78f443623f69104d900f538395444ea46cc8
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-05 22:29:53 +0300

    fixed the syntax error(key should be capitalized)

---
+++ merkle/data/data.go
@@ -0,0 +1,19 @@
+package data
+
+// Normal node
+type NormalNode struct {
+    Data  string
+    Left  *NormalNode   // just an address
+    Right *NormalNode
+}
+
+// Merkle node (hash pointer)
+//root hash is sum of the children hashes
+//if a child hash changes the parent hash also changes and so as the root hash
+type MerkleNode struct {
+    Hash  []byte //32 bytes from sha256       // hash of Left.Hash + Right.Hash + Data
+    Left  *MerkleNode   // still an address, but...
+    Right *MerkleNode
+    // The trick: Left.Hash must equal the actual hash of Left's children
+	Data []byte //only leaf nodes have data
+}
