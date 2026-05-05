9f0b7bd65bcad3345a4d4131e300bce8ea5a4b13
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-05 23:16:44 +0300

    feat:  includes a Parent *MerkleNode field to allow walking up the tree.

--- merkle/data/merkle_node.go
+++ merkle/data/merkle_node.go
@@ -6,9 +6,9 @@ package data
 //root hash is sum of the children hashes
 //if a child hash changes the parent hash also changes and so as the root hash
 type MerkleNode struct {
-    Hash  []byte //32 bytes from sha256       // hash of Left.Hash + Right.Hash + Data
-    Left  *MerkleNode   // still an address, but...
-    Right *MerkleNode
-    // The trick: Left.Hash must equal the actual hash of Left's children
-	Data []byte //only leaf nodes have data
+    Hash   []byte      // hash of Left.Hash + Right.Hash + Data
+    Left   *MerkleNode
+    Right  *MerkleNode
+    Parent *MerkleNode
+    Data   []byte      // only leaf nodes have data
 }
